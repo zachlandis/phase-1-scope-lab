565b56a49fc659d4919cea7ca8142e88d3752bc5
Zach <zachlandis91@gmail.com>
2022-12-17 13:52:44 -0700

    commit tree 43dadf55579afdbf3b52babc15f53b097278dc84

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = 'pete'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'john'
+}
